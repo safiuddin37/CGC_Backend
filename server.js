3f7b98ce0cb3a01e484611b2d75e0c1d13c156a9
stunning-star <mohdsafiuddin99@gmail.com>
2025-03-09 11:44:49 +0530

    backend-1

---
+++ server.js
@@ -0,0 +1,54 @@
+// // console.log("welcome to backend");
+let books = [
+  {
+    id: 1,
+    name: "css&html",
+    price: 400,
+  },
+  {
+    id: 2,
+    name: "java",
+    price: 800,
+  },
+  {
+    id: 3,
+    name: "python",
+    price: 1000,
+  },
+];
+const http = require("http");
+// console.log(http)
+const server = http.createServer((req, res) => {
+  if (req.url.includes("welcome")) {
+    res.statusCode = 200;
+    res.write("Welcome to the server");
+  } else if (req.url.includes("books")) {
+    res.statusCode = 200;
+    b = req.url.split("/");
+    bookid = parseInt(b[b.length - 1]);
+    if (req.url.includes(bookid)) {
+      let selectedbook = books.filter((val) => {
+        if (val.id === bookid) {
+          return val;
+        }
+      });
+      res.write(JSON.stringify(selectedbook));
+    }else{
+        res.write("match not found!")
+    }
+  } else {
+    res.statusCode = 404;
+    res.write("Not Found");
+  }
+  res.end();
+});
+server.listen(5000, () => {
+  console.log("server is UP!!!");
+});
+// let str="bat"
+// if(str.includes("b")){
+//     if(str.includes("a")){
+//         console.log("a found")
+//     }
+// }
+// console.log(selectedbook);
