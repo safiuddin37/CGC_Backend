d750b0e018ceb9cd3f8ea7945fb4faa15d0d63cc
stunning-star <mohdsafiuddin99@gmail.com>
2025-03-23 00:36:15 +0530

    backend-mongodb(Users)

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,9 +1,17 @@
 const userModel = require("../models/userModel");
-const { responseObjGenerator, hashPassword ,comparePassword } = require("../utils/utils");
-
+const {
+  responseObjGenerator,
+  hashPassword,
+  comparePassword,
+} = require("../utils/utils");
 const getUsers = async (req, res) => {
-  const users = await userModel.find();
-  res.status(200).json(users);
+  try {
+    const users = await userModel.find();
+    res.status(200).json(users);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
 };
 const addUser = async (req, res) => {
   try {
@@ -13,16 +21,14 @@ const addUser = async (req, res) => {
     await user.save();
     let resObj = responseObjGenerator(true, "User Added Successfully!", user);
     res.status(201).json(resObj);
-  } catch (err) {
-    if (err.errorResponse.errmsg.includes("duplicate key")) {
-      res
+  } catch (e) {
+    console.error(e);
+    if (e.code === 11000 && e.keyValue && e.keyValue.email) {
+      return res
         .status(400)
-        .json({ message: "Email Already Exists!", success: false });
-    } else {
-      res
-        .status(500)
-        .json({ message: "Internal Server Error!", success: false });
+        .json({ success: false, message: "Email Already Exists!" });
     }
+    res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 };
 const updateUser = async (req, res) => {
@@ -32,81 +38,90 @@ const updateUser = async (req, res) => {
     const user = await userModel.findOneAndUpdate({ _id: userId }, data, {
       new: true,
     });
-    // const prd = await ProductModel.findOne({ _id: productId });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User Not Found!" });
+    }
     let resObj = responseObjGenerator(true, "User Updated Successfully!", user);
     res.status(200).json(resObj);
   } catch (e) {
-    console.log(e);
-    let resObj = responseObjGenerator(false);
-    res.status(500).json(resObj);
+    console.error(e);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 };
-
 const updateCompUser = async (req, res) => {
   try {
     const userId = req.params.id;
-    const data = req.body;
-    const user = await userModel.updateOne(
+    let data = req.body;
+    if (data.password) {
+      data.password = await hashPassword(data.password); // Hash password if updated
+    }
+    const user = await userModel.findOneAndUpdate(
       { _id: userId },
-      {
-        $set: {
-          name: data.name,
-          email: data.email,
-          password: data.password,
-          usertype: data.usertype,
-        },
-      },
+      { $set: data },
       { new: true }
     );
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User Not Found!" });
+    }
     let resObj = responseObjGenerator(true, "User Updated Successfully!", user);
     res.status(200).json(resObj);
   } catch (e) {
-    console.log(e);
-    let resObj = responseObjGenerator(false);
-    res.status(500).json(resObj);
+    console.error(e);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 };
 const deleteUser = async (req, res) => {
   try {
     let userId = req.params.id;
-    await userModel.deleteOne({ _id: userId });
+    const result = await userModel.deleteOne({ _id: userId });
+    if (result.deletedCount === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User Not Found!" });
+    }
     let resObj = responseObjGenerator(true, "User Deleted Successfully!");
     res.status(200).json(resObj);
   } catch (e) {
-    console.log(e);
-    let resObj = responseObjGenerator(false);
-    res.status(500).json(resObj);
+    console.error(e);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 };
 const signin = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await userModel.findOne({ email: email, password: password });
-    if (user) {
-      const isPasswordMatches = await comparePassword(password, user.password);
-      if (isPasswordMatches) {
-        // const token = generateToken({ email, name: user.name, id: user._id, userRole: user.userRole });
-        return res.status(200).json({
-          message: "User Loggedin Successfully!",
-          success: true,
-          data: {
-            name: user.name,
-            email: user.email,
-            _id: user._id,
-            // token,
-          },
-        });
-      }
-    } else {
-      let resObj = responseObjGenerator(false, "Invalid Email or Password!");
-      res.status(400).json(resObj);
+    const user = await userModel.findOne({ email: email });
+    if (!user) {
+      return res
+        .status(400)
+        .json(responseObjGenerator(false, "Invalid Email or Password!"));
     }
+    const isPasswordMatches = await comparePassword(password, user.password);
+    if (!isPasswordMatches) {
+      return res
+        .status(400)
+        .json(responseObjGenerator(false, "Invalid Email or Password!"));
+    }
+    // const token = generateToken({ email, name: user.name, id: user._id, userRole: user.userRole });
+    res.status(200).json({
+      success: true,
+      message: "User Logged in Successfully!",
+      data: {
+        name: user.name,
+        email: user.email,
+        _id: user._id,
+        // token,
+      },
+    });
   } catch (e) {
-    console.log(e);
-    let resObj = responseObjGenerator(false);
-    res.status(500).json(resObj);
+    console.error(e);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 };
+
 module.exports = {
   getUsers,
   addUser,
--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const responseObjGenerator = (success, message, data) => {
   let resObj = {};
   resObj.success = success;
